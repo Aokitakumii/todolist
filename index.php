0ba259bc4016221f0082094923760b2f2fc6ace1
tukky523 <k014c1115@it-neec.jp>
2016-05-30 13:09:51 +0900

    YOROSHIKUgit add .

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
+<!DOCTYPE html>
+
 <html>
+<head>
+<meta charset="UTF-8">
+<title>todolist</title>
+</head>
+<body>
+  <?php
+    $dsn = 'mysql:dbname=todolist:host=localhost:charset=u'
+    $user = 'root';
+    $password = 'root';
+    $dbh = new PDO($dsn, $user, password);
+    $dbh->query('SET NAMES utf8');
+    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  ?>
 <INPUT type="text" name="txt">
 <button type="btn">add</button>
 <HR>
+</body>
 </html>
