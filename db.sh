5a1ab3be0ef9af4daecbf048e95a9e216c73af64
Nakamura <k014c1104@it-neec.jp>
2016-05-30 13:47:08 +0900

    edited

--- db.sh
+++ db.sh
@@ -4,7 +4,7 @@ mysql -uroot -e "drop database todolist"
 
 mysql -uroot -e "create database todolist character set UTF8;"
 
-mysql -uroot -e "create table todolist.todo(id int primary key,title varchar(30));"
+mysql -uroot -e "create table todolist.todo(id int AUTO_INCREMENT primary key,title varchar(30));"
 
 mysql -uroot -e "insert into todolist.todo(id,title)values(0,'test')"
 
